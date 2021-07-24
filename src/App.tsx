2a7090efc199d5ce15a13c617c80adb10502064a
Tim O'Malley <tomalley24@gmail.com>
2021-07-24 13:18:16 -0500

    basic deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import './App.css';
-import Nav from './compenents/Nav'
+//import Nav from './compenents/Nav'
 
 function App() {
   return (
   <Router>
     <div>
-      <Nav />
       <h1 className = "text-4xl text-center text-blue-500">Hello World</h1>
     </div>
   </Router>
