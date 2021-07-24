12c99fdc50556066e309d2333b7502fb272cc860
Tim O'Malley <tomalley24@gmail.com>
2021-07-24 13:31:17 -0500

    still trying basic deploy

---
+++ server.ts
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const routes = require("./routes");
+const sequelize = require("./config/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "./client/build"));
+// });
+
+// turn on routes
+app.use(routes);
+
+// turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => {
+    console.log(`Now listening on port ${PORT}!`);
+  });
+});
