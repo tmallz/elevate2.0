100757701b3bc031e3060a02ccdbadade5f69a8d
Tim O'Malley <tomalley24@gmail.com>
2021-07-25 15:25:59 -0500

    added login form page and styling, need to add functionality still

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import './App.css';
 import Nav from './compenents/Nav'
 import Homepage from './pages/homepage'
+import Login from './pages/login'
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <Router>
         <Route exact path ='/'>
           <Homepage />
+        </Route>
+        <Route exact path ='/login'>
+          <Login />
         </Route>
       </Router>
     </div>
